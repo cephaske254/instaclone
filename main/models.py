0eeba54c9f82d88d3467bca21781ea74615ab46c
cephaske254 <cephaske254@gmail.com>
2020-05-30 22:29:15 +0300

    created model for posts

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    image = models.ImageField( upload_to='posts',default='avatar.jpg')
+    caption = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
