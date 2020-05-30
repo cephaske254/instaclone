3e8ddb1c98941f2f310784e8eae87e1fb5683e38
cephaske254 <cephaske254@gmail.com>
2020-05-30 22:31:55 +0300

    created model for comments

--- main/models.py
+++ main/models.py
@@ -2,8 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
 class Post(models.Model):
     image = models.ImageField( upload_to='posts',default='avatar.jpg')
     caption = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
+
+class Comments(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    comment = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
