93362119719d7b492460fcc75d5053eb90324c11
cephaske254 <cephaske254@gmail.com>
2020-06-01 23:27:46 +0300

    click to view profile

--- accounts/models.py
+++ accounts/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE, related_name='profile')
     bio = models.TextField()
-    profile_image = models.ImageField(upload_to='profiles', null=True, blank=True)
+    profile_image = models.ImageField(upload_to='media/profiles', null=True, blank=True)
 
     def __str__(self):
         return self.user.username
